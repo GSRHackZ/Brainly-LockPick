63b73054126700f9767e4e4c37ebc9bea881050b
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2022-10-02 21:52:55 -0400

    Create lockpick_user_traction.js

---
+++ lockpick_user_traction.js
@@ -0,0 +1,34 @@
+// This is just for the script to gain traction when people search for edgenuity scripts specifically written in javascript
+
+// GitHub doesn't seem to understand that the script (obfuscated) is written in javascript.
+
+alert("This is useless for users but useful for me and people looking for this script or any brainly script. 😊");
+
+// Basically, ignore this file. Go check out lockPick.js instead. That file contains the actual script. ✔
+
+// This file increases user traction and visibility of script so it sticks out among the other (dead / patched / fake) scripts.
+
+
+
+
+
+
+
+
+// um.....Why are u still here....reading? Please go away...now 🙂
+
+
+//ok.... this is weird.... go ahead, read my hashes I guess.....
+
+
+/*
+brainly,free, unlock answers, free answers, brainly lockpick, 
+free answers brainly free, unpatchable script for brainly, brainly answer grabber, 
+free brainly textbook answers
+*/
+
+
+
+// I hope you enjoyed reading that..... 😑
+
+// Bye 👋
